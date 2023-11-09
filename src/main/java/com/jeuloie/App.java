e8ba836667789f942f3bbc73b77cc4a3897e2a76
ouzammad <yassineouzammad17@gmail.com>
2023-11-09 10:04:55 +0100

    first commit

---
+++ src/main/java/com/jeuloie/App.java
@@ -0,0 +1,20 @@
+package com.jeuloie;
+
+import java.util.Random;
+
+public class App {
+
+    public static void main(String[] args) {
+        int totalcase = 20;
+        int valeurlance;
+        int caserestante;
+        int caseactuelle = caseactuelle + valeurlance;
+        for (int numlancer = 1; numlancer <= 5; numlancer++) {
+            Random generateur = new Random();
+            valeurlance = generateur.nextInt(6) + 1;
+            System.out.println(valeurlance);
+            System.out.print("case restante");
+        }
+    }
+
+}
